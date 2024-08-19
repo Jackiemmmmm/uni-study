04e66c30cbb25de3e064c1d0f0751c6b0d3d5424
jackie.tu <jackie@openex.xyz>
2024-08-19 12:51:08 +1000

    feat: add 881 test

---
+++ week5-0819-0823/881/lab-test.py
@@ -0,0 +1,47 @@
+# Question 1
+# for value in range(1, 11):
+#     print('{0:>2} + {1:>2} = {2:>2}'.format(value, value, value + value))
+
+
+# Question 2
+# for value in range(1, 11):
+#     if (value < 10):
+#         print(value, end=' - ')
+#     else:
+#         print('{0}.'.format(value))
+
+
+# Question 3
+# for value in range(1, 11):
+#     if value < 10:
+#         print(value, end=" : ")
+#     else:
+#         print(value, end="")
+
+
+# Question 4
+# for value in range(1, 11):
+#     if value % 2 == 0:
+#         if value < 10:
+#             print(value, end=", ")
+#         else:
+#             print(value, end=".")
+
+
+# Question 5
+# for value in range(1, 11):
+#     if value % 2:
+#         if value < 9:
+#             print(value, end="; ")
+#         else:
+#             print(value, end=".")
+
+
+# Question 6
+# for value in range(2, 11):
+#     newString = ""
+#     for childValue in range(1, value):
+#         newString += str(childValue)
+#     print(newString)
+
+
