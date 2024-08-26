777db5661bce44c91a71279cf115a1f90e6692bb
jackie.tu <jackie@openex.xyz>
2024-08-26 17:26:09 +1000

    feat: add 884 lab-test & update 881 lab-test on Week6OC

--- week6-0826-0830/881/lab-test.py
+++ week6-0826-0830/881/lab-test.py
@@ -84,3 +84,65 @@
 # Question 5
 
 # 2, 8, 4, 10, 6, 0, 12
+
+
+# Insertion sort Algorithm
+
+# list = [60, 10, 90, 50, 100, 80, 70, 30, 40, 20]
+# count = 0
+# length = len(list)
+# print("Before Sort Array: {0}".format(list))
+# for value in range(1, length):
+#     key = value
+#     while key > 0 and list[key - 1] > list[key]:
+#         newValue = list[key]
+#         list[key] = list[key - 1]
+#         list[key - 1] = newValue
+#         key -= 1
+#         count += 1
+
+# print("Count: {0}".format(count))
+# print("After Sort Array: {0}".format(list))
+
+
+# Selection sort Algorithm
+
+# list = [60, 10, 90, 50, 100, 80, 70, 30, 40, 20]
+# count = 0
+# length = len(list)
+# print("Before Sort Array: {0}".format(list))
+# for value in range(0, length - 1):
+#     min = value
+#     for innerValue in range(value, length):
+#         if list[min] > list[innerValue]:
+#             min = innerValue
+#             count += 1
+#     cacheValue = list[value]
+#     list[value] = list[min]
+#     list[min] = cacheValue
+
+# print("Count: {0}".format(count))
+# print("After Sort Array: {0}".format(list))
+
+
+# Bubble sort Algorithm
+
+# list = [60, 10, 90, 50, 100, 80, 70, 30, 40, 20]
+# count = 0
+# length = len(list)
+# print("Before Sort Array: {0}".format(list))
+
+# for i in range(0, length - 1):
+#     max = i
+#     for j in range(1, length):
+#         if list[j] < list[j - 1]:
+#             max = j - 1
+#             cacheValue = list[j]
+#             list[j] = list[max]
+#             list[j - 1] = cacheValue
+#             count += 1
+#         print("After Sort Array: {0}".format(list))
+            
+# print("Count: {0}".format(count))
+# print("After Sort Array: {0}".format(list))
+
